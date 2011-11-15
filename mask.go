e4bee44674617e9d9003720cce967ae6dd0b0fd7
Ryan W Sims <rwsims@gmail.com>
2011-11-15 09:37:51 -0500

    Added notes on algorithm

--- mask.go
+++ mask.go
@@ -6,6 +6,14 @@ import (
     "os"
 )
 
+// left <- first digit
+// right <- left + 1
+// increment right until 14th digit
+//    if invalid char found, restart with next possible
+// check right, right + 1 digit, right + 2 digits
+// keep track of last character output, if check succeeds output from pos to right
+// start again with next character after right
+
 func main() {
     reader := bufio.NewReader(os.Stdin)
     line, err := reader.ReadString('\n')
