0d62205054ffc22081aea2fb281b1145be4cada7
Ryan W Sims <rwsims@gmail.com>
2011-11-16 12:09:29 -0500

    Implemented scanning for 14th digit: masked slightly by gofix changes.

--- mask.go
+++ mask.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "regexp"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"unicode/utf8"
 )
 
 var (
-    digit = regexp.MustCompile("^[0-9].*")
-    validCardRune = regexp.MustCompile("[\\- 0-9]")
+	digit         = regexp.MustCompile("^[0-9].*")
+	validCardRune = regexp.MustCompile(`^([\-0-9 ]).*`)
 )
+
+func assert(cond bool, message string) {
+	if !cond {
+		panic(message)
+	}
+}
+
+func debug(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format+"\n", args...)
+}
 
 // left <- first digit
 // right <- left + 1
@@ -21,47 +33,73 @@ var (
 // start again with next character after right
 
 type CardMasker interface {
-    Mask(line string) string
+	Mask(line string) string
 }
 
-type IdentityMasker struct {}
+type IdentityMasker struct{}
 
 func (masker IdentityMasker) Mask(line string) string {
-    return line
+	return line
 }
 
-type IterativeMasker struct {}
+type IterativeMasker struct{}
 
 func (masker IterativeMasker) Mask(line string) string {
-    slice := make([]int, len(line))
-    for idx, rune := range line {
-        slice[idx] = rune
-    }
-    return string(slice)
+	src := utf8.NewString(line)
+	output := make([]int, len(line))
+
+	left := firstDigitAfter(line, 0)
+	right := findNthDigitAfter(line, left, 14)
+	debug("left: %d, right: %d\n", left, right)
+	if right > 0 {
+		debug("number: %s\n", line[left:right])
+	}
+	nextOut := 0
+	for nextOut < left {
+		output[nextOut] = src.At(nextOut)
+		nextOut++
+	}
+	assert(left == nextOut, "next output must be first digit")
+
+	return string(output)
 }
 
 // Return the position of the first digit with a position >= start.
 func firstDigitAfter(runes string, start int) int {
-    pos := start
-    for !digit.MatchString(runes[pos:]) {
-        pos++;
-    }
-    return pos
+	pos := start
+	for pos < len(runes) && !digit.MatchString(runes[pos:]) {
+		pos++
+	}
+	return pos
 }
 
-func handleLine(line string, masker CardMasker) string {
-    return masker.Mask(line)
+// Return position of nth digit after start. Digits may be interspersed with
+// spaces and dashes, but if an invalid card rune is found before n, return -1;
+func findNthDigitAfter(runes string, start int, n int) int {
+	pos := start
+	for n > 1 {
+		debug("n: %d, runes: %s", n, runes[pos:])
+		if !validCardRune.MatchString(runes[pos:]) {
+			debug("invalid rune starts '%s'", runes[pos:])
+			return -1
+		}
+		if digit.MatchString(runes[pos:]) {
+			n--
+		}
+		pos++
+	}
+	return pos
 }
 
 func main() {
-    reader := bufio.NewReader(os.Stdin)
-    line, err := reader.ReadString('\n')
-    var masker IterativeMasker
-    for err == nil {
-        fmt.Print(handleLine(line, masker))
-        line, err = reader.ReadString('\n')
-    }
-    if err != os.EOF {
-        fmt.Fprintf(os.Stderr, "Error: %s\n", err);
-    }
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+	var masker IterativeMasker
+	for err == nil {
+		fmt.Print(masker.Mask(line))
+		line, err = reader.ReadString('\n')
+	}
+	if err != io.EOF {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+	}
 }
